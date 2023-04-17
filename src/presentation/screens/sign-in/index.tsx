a54f20ef5deb8e7f6c50851e978bdbf5f7f74250
diegoalvescf <dalvesdocs@gmail.com>
2023-04-17 19:28:58 -0300

    feat: finish signin screen

--- src/presentation/screens/sign-in/index.tsx
+++ src/presentation/screens/sign-in/index.tsx
@@ -1,66 +1,82 @@
-import { VStack, Image, Center, Text, Heading } from 'native-base';
+import { Center, Heading, Image, ScrollView, Text, VStack } from 'native-base';
 
 import BackgroundImg from '@assets/background.png';
 import LogoImg from '@assets/logo-goal.png';
-import { InputComponent } from '@components/input';
 import { ButtonComponent } from '@components/button';
+import { InputComponent } from '@components/input';
 
 export const SignInScreen: React.FC = ({}) => {
   return (
-    <VStack
-      flex={1}
-      bg={'gray.700'}
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
     >
-      <Image
-        source={BackgroundImg}
-        alt='Pessoas treinando'
-        resizeMode='contain'
-        position={'absolute'}
-      />
-
-      <Center my={24}>
+      <VStack
+        flex={1}
+        bg={'gray.700'}
+        px={10}
+      >
         <Image
-          source={LogoImg}
-          width={170}
-          height={70}
-          alt='logo goal'
-        />
-        <Text
-          color={'gray.100'}
-          fontSize={'sm'}
-        >
-          Treine sem limites
-        </Text>
-      </Center>
-
-      <Center p={10}>
-        <Heading
-          color={'gray.100'}
-          fontSize={'xl'}
-          mb={6}
-          fontFamily={'heading'}
-        >
-          Acesse sua conta
-        </Heading>
-
-        <InputComponent
-          placeholder='Digite seu email'
-          autoCapitalize='none'
-          keyboardType='email-address'
+          source={BackgroundImg}
+          alt='Pessoas treinando'
+          resizeMode='contain'
+          position={'absolute'}
         />
 
-        <InputComponent
-          placeholder='Digite sua senha'
-          secureTextEntry
-        />
+        <Center my={24}>
+          <Image
+            source={LogoImg}
+            width={170}
+            height={70}
+            alt='logo goal'
+          />
+          <Text
+            color={'gray.100'}
+            fontSize={'sm'}
+          >
+            Treine sem limites
+          </Text>
+        </Center>
 
-        <ButtonComponent title='Acessar' />
+        <Center>
+          <Heading
+            color={'gray.100'}
+            fontSize={'xl'}
+            mb={6}
+            fontFamily={'heading'}
+          >
+            Acesse sua conta
+          </Heading>
 
-        <ButtonComponent
-          title='Criar conta'
-          variant={'outline'}
-        />
-      </Center>
-    </VStack>
+          <InputComponent
+            placeholder='Digite seu email'
+            autoCapitalize='none'
+            keyboardType='email-address'
+          />
+
+          <InputComponent
+            placeholder='Digite sua senha'
+            secureTextEntry
+          />
+
+          <ButtonComponent title='Acessar' />
+        </Center>
+
+        <Center mt={24}>
+          <Text
+            color={'gray.100'}
+            fontSize={'sm'}
+            fontFamily={'body'}
+            mb={3}
+          >
+            Ainda não tem acesso?
+          </Text>
+          <ButtonComponent
+            title='Criar conta'
+            variant={'outline'}
+          />
+        </Center>
+      </VStack>
+    </ScrollView>
   );
 };
